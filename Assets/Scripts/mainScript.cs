4e6cb625b538be25a1060cdc2a94b99ace4223d2
nikiteezy <nikiteezy@gmail.com>
2017-08-15 00:16:06 +0300

    First version game on GitHub

---
+++ Assets/Scripts/mainScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class mainScript : MonoBehaviour {
+	public Text scoreLbl;
+	public int timer;
+	public float score = 0.0001F;
+	const float bitcoinRate = 0.000004F;
+	public int ratio = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreTimeUp ();
+		timeScoreUp ();
+
+
+	}
+	void scoreTimeUp()
+	{
+		float bitcoinScore = (float)(score*bitcoinRate);
+		scoreLbl.text = "" + bitcoinScore;
+	}
+	void timeScoreUp()
+	{
+		if (timer < (int)Time.time) 
+		{
+			timer = (int)Time.time;
+			score++;
+		} 
+	}
+
+	public void OnClickCoin()
+	{
+		score += ratio;
+	}
+}
---
+++ Assets/Scripts/testBtn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testBtn : MonoBehaviour {
+
+	public int count = 0;
+	public void Cl(){
+		count++;
+	}
+}

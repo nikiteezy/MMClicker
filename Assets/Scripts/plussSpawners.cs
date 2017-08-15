b8778ac1e137b1200a2facd066a45dfa5139277d
nikiteezy <nikiteezy@gmail.com>
2017-08-15 21:04:28 +0300

    Now prefab of plus spawns on canvas

--- Assets/Scripts/plussSpawners.cs
+++ Assets/Scripts/plussSpawners.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class plussSpawners : MonoBehaviour {
-
+	public GameObject Canvas;
 	public GameObject plus;
 //	public Vector3 spawnValues;
 
 	public void plusSpawner()
 	{
 		Vector3 spawnPosition = new Vector3 (0,0,0);
-		Instantiate (plus, spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation); 
+		plus = Instantiate (plus, spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation) as GameObject;
+		plus.transform.SetParent (Canvas.transform, false);
 	}
 }

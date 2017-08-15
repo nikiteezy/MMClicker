d56b7ccacf7a9a5d5e735a877cbc48214152bf90
nikiteezy <nikiteezy@gmail.com>
2017-08-15 03:03:59 +0300

    add plus prefab and spawner cod(need modifications)

--- Assets/Scripts/mainScript.cs
+++ Assets/Scripts/mainScript.cs
@@ -3,30 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class mainScript : MonoBehaviour {
-	public Text scoreLbl;
-	public int timer;
-	public float score = 0.0001F;
-	const float bitcoinRate = 0.000004F;
-	public int ratio = 5;
-	// Use this for initialization
-	void Start () {
+	public Text scoreLbl;				//Лэйбл вывода очков
+
+
+
+
+	public int timer;					//Таймер, отсчитывает секунды со старта игры
+	public float score = 0.0001F;		//Главные очки
+	const float bitcoinRate = 0.000004F;//Отношение биткоина к рублю
+	public int ratio = 5;				//Коэффициент для нажатия
+
+	void Start () 
+	{
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Update () 
+	{
 		scoreTimeUp ();
 		timeScoreUp ();
-
-
 	}
+
 	void scoreTimeUp()
 	{
 		float bitcoinScore = (float)(score*bitcoinRate);
 		scoreLbl.text = "" + bitcoinScore;
 	}
+
 	void timeScoreUp()
 	{
 		if (timer < (int)Time.time) 
---
+++ Assets/Scripts/plussSpawners.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plussSpawners : MonoBehaviour {
+
+	public GameObject plus;
+//	public Vector3 spawnValues;
+
+	public void plusSpawner()
+	{
+		Vector3 spawnPosition = new Vector3 (0,0,0);
+		Instantiate (plus, spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation); 
+	}
+}
